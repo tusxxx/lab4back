e9e5ae9faba77842781590a4c23574857c669b9e
Игнат Мустафаев <tusxdie@gmail.com>
2024-03-28 16:17:11 +0700

    asd

--- src/main/kotlin/com/example/lab4back/ui/TodoController.kt
+++ src/main/kotlin/com/example/lab4back/ui/TodoController.kt
@@ -1,17 +1,15 @@
 package com.example.lab4back.ui
 
+import com.example.lab4back.data.TodoItem
 import com.example.lab4back.data.TodoRepository
 import jakarta.websocket.server.PathParam
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.*
 
 @Controller
-class TodoController {
-
-    @Autowired lateinit var todoRepository: TodoRepository
+class TodoController(private val todoRepository: TodoRepository) {
     @GetMapping("/")
     fun index(model: Model): String {
         model.addAttribute("todos", todoRepository.findAll())
@@ -19,12 +17,24 @@ class TodoController {
     }
 
     @GetMapping("/switchState/{todoId}")
-    fun switchState(model: Model, @PathVariable todoId: Int): String {
-        val todoItem = todoRepository.getReferenceById(todoId.toLong())
+    fun switchState(model: Model, @PathVariable todoId: Long): String {
+        val todoItem = todoRepository.findById(todoId).orElseThrow { NoSuchElementException("Todo not found") }
         val newTodoItem = todoItem.copy(state = !todoItem.state)
-        todoRepository.delete(todoItem)
         todoRepository.save(newTodoItem)
-        model.addAttribute("todos", todoRepository.findAll())
-        return "index"
+        return "redirect:/"
+    }
+
+    @GetMapping("/delete/{todoId}")
+    fun delete(model: Model, @PathVariable todoId: Long): String {
+        todoRepository.deleteById(todoId)
+        return "redirect:/"
+    }
+
+    @PostMapping("/add")
+    fun add(model: Model, @ModelAttribute todoItem: TodoItem): String {
+        if (todoRepository.findAll().map { it.id }.containsAll(todoItem.subtasksIds.map { it.toLong() })) {
+            todoRepository.save(todoItem)
+        }
+        return "redirect:/"
     }
 }
